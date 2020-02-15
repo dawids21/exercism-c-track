b81cd82cf284cae98a7f05756eba9320f5998022
Dawid <daw020101@gmail.com>
2020-02-15 17:47:25 +0100

    Repair bug with spaces at the end

--- atbash-cipher/src/atbash_cipher.c
+++ atbash-cipher/src/atbash_cipher.c
@@ -2,31 +2,41 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdbool.h>
 
 const char ALPHABET[] = "abcdefghijklmnopqrstuvwxyz";
 const char CIPHER[] = "zyxwvutsrqponmlkjihgfedcba";
 
 char *atbash_encode(const char *input)
 {
-    char* result = (char*) malloc(MAX_LEN);
+    char* result = (char*)malloc(MAX_LEN);
     int length = 0;
-    int char_num = 0;
+    int char_num_space = 0;
     for (int i = 0; input[i] != '\0'; i++)
     {
-        if(isalpha(input[i]))
+        if (isalnum(input[i]))
         {
-            /*  find index of letter in ALPHABET using ASCII postion
-                and add letter at the same position in CIPHER to result*/
-            result[length] = CIPHER[tolower(input[i]) - 'a'];
-            length++;
-            char_num++;
-        }
-        //  space every 5 letters
-        if (char_num == 5)
-        {
-            result[length] = ' ';
-            length++;
-            char_num = 0;
+            //  space every 5 letters but only if there are more letters
+            if (char_num_space == 5)
+            {
+                result[length] = ' ';
+                length++;
+                char_num_space = 0;
+            }
+            if(isalpha(input[i]))
+            {
+                /*  find index of letter in ALPHABET and add letter at
+                    the same position in CIPHER to result*/
+                result[length] = CIPHER[strchr(ALPHABET, tolower(input[i]))-ALPHABET];
+                length++;
+                char_num_space++;
+            }
+            else if (isdigit(input[i]))
+            {
+                result[length] = input[i];
+                length++;
+                char_num_space++;
+            }
         }
     }
     result[length] = '\0';
@@ -44,6 +54,11 @@ char *atbash_decode(const char *input)
             /*  find index of letter in CIPHER and add letter at the same position
                 in ALPHABET to result*/
             result[length] = ALPHABET[strchr(CIPHER, tolower(input[i]))-CIPHER];
+            length++;
+        }
+        else if (isdigit(input[i]))
+        {
+            result[length] = input[i];
             length++;
         }
     }
