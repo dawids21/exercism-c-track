648cd6067520cfb68b15dc078db329cd8118a947
Dawid <daw020101@gmail.com>
2020-02-15 17:23:28 +0100

    First try (bug with splitting)

--- atbash-cipher/src/atbash_cipher.c
+++ atbash-cipher/src/atbash_cipher.c
@@ -1,1 +1,39 @@
 #include "atbash_cipher.h"
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+const char ALPHABET[] = "abcdefghijklmnopqrstuvwxyz";
+const char CIPHER[] = "zyxwvutsrqponmlkjihgfedcba";
+
+char *atbash_encode(const char *input)
+{
+    char* result = (char*) malloc(strlen(input)*sizeof(char)+1);
+    int length = 0;
+    for (int i = 0; input[i] != '\0'; i++)
+    {
+        if(isalpha(input[i]))
+        {
+            result[length] = CIPHER[tolower(input[i]) - 'a'];
+            length++;
+        }
+    }
+    result[length] = '\0';
+    return result;
+}
+
+char *atbash_decode(const char *input)
+{
+    char* result = (char*) malloc(strlen(input)*sizeof(char)+1);
+    int length = 0;
+    for (int i = 0; input[i] != '\0'; i++)
+    {
+        if(isalpha(input[i]))
+        {
+            result[length] = ALPHABET[strchr(CIPHER, tolower(input[i]))-CIPHER];
+            length++;
+        }
+    }
+    result[length] = '\0';
+    return result;
+}
