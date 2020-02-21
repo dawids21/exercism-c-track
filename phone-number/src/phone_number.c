7fa63b04bae6ad1f914fd92ed53837cfcdff4762
Dawid <daw020101@gmail.com>
2020-02-21 13:22:26 +0100

    First test

--- phone-number/src/phone_number.c
+++ phone-number/src/phone_number.c
@@ -1,1 +1,31 @@
 #include "phone_number.h"
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+static int extract_digits(const char* input, char* output);
+
+char *phone_number_clean(const char *input)
+{
+    char* phone_number = (char*)malloc(sizeof(char)*BUFFER);
+    int length = extract_digits(input, phone_number);
+    if (length != 10)
+    {
+        strcpy(phone_number, "00000000000");
+    }
+    return phone_number;
+}
+
+static int extract_digits(const char* input, char* output)
+{
+    int length = 0;
+    for (size_t i = 0; i < strlen(input); i++)
+    {
+        if (isdigit(input[i]))
+        {
+            output[length] = input[i];
+            length++;
+        }
+    }
+    return length;
+}
--- phone-number/src/phone_number.h
+++ phone-number/src/phone_number.h
@@ -1,6 +1,8 @@
 #ifndef PHONE_NUMBER_H
 #define PHONE_NUMBER_H
 
+#define BUFFER 20
+
 char *phone_number_clean(const char *input);
 
 #endif
