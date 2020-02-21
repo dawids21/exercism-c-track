630b34796cf8996016610ee14056dc273bbe6652
Dawid <daw020101@gmail.com>
2020-02-21 13:48:52 +0100

    Some refactor

--- phone-number/src/phone_number.c
+++ phone-number/src/phone_number.c
@@ -7,26 +7,23 @@ static int extract_digits(const char* input, char* output);
 
 char *phone_number_clean(const char *input)
 {
-    char* phone_number = (char*)malloc(sizeof(char)*BUFFER);
+    char* phone_number = (char*)malloc(sizeof(char)*strlen(input));
     int length = extract_digits(input, phone_number);
-    if (length != 10)
+
+    //Number is valid if has 11 digits and first digit is 1
+    if (length == 11 && phone_number[0] == '1')
     {
-        //Number is valid if has 11 digits and first digit is 1
-        if (length == 11 && phone_number[0] == '1')
-        {
-            strcpy(phone_number, &phone_number[1]);
-        }
-        else
-        {
-            strcpy(phone_number, "0000000000");
-        }
+        strcpy(phone_number, &phone_number[1]);
+        length--;
     }
-    //Area and exchange code can't be 0 or 1
-    if (phone_number[0] == '0' || phone_number[0] == '1' ||
+
+    //Number has 10 digits and area and exchange code can't be 0 or 1
+    if (length != 10 || phone_number[0] == '0' || phone_number[0] == '1' ||
         phone_number[3] == '0' || phone_number[3] == '1')
     {
         strcpy(phone_number, "0000000000");
     }
+
     return phone_number;
 }
 
--- phone-number/src/phone_number.h
+++ phone-number/src/phone_number.h
@@ -1,8 +1,6 @@
 #ifndef PHONE_NUMBER_H
 #define PHONE_NUMBER_H
 
-#define BUFFER 20
-
 char *phone_number_clean(const char *input);
 
 #endif
