b664cda9be2f5d1733e6793c13082e41c313ef20
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-03-23 08:17:13 +0100

    Pass second test

--- pascals-triangle/src/pascals_triangle.c
+++ pascals-triangle/src/pascals_triangle.c
@@ -1,6 +1,6 @@
 #include "pascals_triangle.h"
 
-static int calculate_row_size(int num_of_rows);
+static void add_numbers(size_t **triangle, int num_of_rows);
 
 void free_triangle(size_t **triangle, size_t num_of_rows)
 {
@@ -25,19 +25,32 @@ size_t **create_triangle(int num_of_rows)
     if (num_of_rows == 0)
     {
         triangle = (size_t **)malloc(sizeof(size_t *));
-        triangle[0] = (size_t *)malloc(sizeof(size_t));
+        triangle[0] = (size_t *)calloc(1, sizeof(size_t));
         return triangle;
     }
     triangle = (size_t **)malloc(num_of_rows * sizeof(size_t *));
-    int row_size = calculate_row_size(num_of_rows);
     for (int i = 0; i < num_of_rows; i++)
     {
-        triangle[i] = (size_t *)malloc(row_size * sizeof(size_t));
+        triangle[i] = (size_t *)calloc(num_of_rows, sizeof(size_t));
     }
+    add_numbers(triangle, num_of_rows);
     return triangle;
 }
 
-static int calculate_row_size(int num_of_rows)
+static void add_numbers(size_t **triangle, int num_of_rows)
 {
-    return 2 * num_of_rows + 1;
+    triangle[0][0] = 1;
+    for (int i = 1; i < num_of_rows; i++)
+    {
+        triangle[i][0] = 1;
+        for (int j = 1; j < num_of_rows; j++)
+        {
+            int number = triangle[i - 1][j - 1] + triangle[i - 1][j];
+            triangle[i][j] = number;
+            if (number == 1)
+            {
+                break;
+            }
+        }
+    }
 }
