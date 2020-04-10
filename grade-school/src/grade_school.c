afd99a01f6778dde89a0efb0088735ef29237356
dawids21 <dawid.stasiak21@gmail.com>
2020-04-10 14:50:34 +0200

    Refactor

--- grade-school/src/grade_school.c
+++ grade-school/src/grade_school.c
@@ -4,9 +4,9 @@
 static roster_t roster = {
     .count = 0};
 
+static int find_place_for_student(char *name, uint8_t grade);
 static int get_grade_start_index(uint8_t grade);
 static int get_grade_end_index(uint8_t grade, int grade_start_index);
-static int find_place_for_student(char *name, uint8_t grade);
 
 roster_t get_roster(void)
 {
@@ -25,10 +25,13 @@ bool add_student(char *name, uint8_t grade)
         return false;
     }
     int place_for_student = find_place_for_student(name, grade);
+
+    /*Shift students to the right to make room for another one*/
     for (int i = roster.count - 1; i >= place_for_student; i--)
     {
         memcpy(&roster.students[i + 1], &roster.students[i], sizeof(student_t));
     }
+
     strcpy(roster.students[place_for_student].name, name);
     roster.students[place_for_student].grade = grade;
     roster.count++;
@@ -40,17 +43,20 @@ roster_t get_grade(uint8_t grade)
     roster_t grade_roster;
     memset(&grade_roster, 0, sizeof(roster_t));
     int grade_start_index = get_grade_start_index(grade);
+
+    /*If grade is not present return empty roster*/
     if (grade_start_index == -1)
     {
         return grade_roster;
     }
-    for (int i = grade_start_index; (size_t)i < roster.count; i++)
+
+    for (int i = grade_start_index; i < (int)roster.count; i++)
     {
         if (roster.students[i].grade == grade)
         {
-            strcpy(grade_roster.students[grade_roster.count].name,
-                   roster.students[i].name);
-            grade_roster.students[grade_roster.count].grade = grade;
+            memcpy(&grade_roster.students[grade_roster.count],
+                   &roster.students[i],
+                   sizeof(student_t));
             grade_roster.count++;
         }
         else
@@ -61,23 +67,12 @@ roster_t get_grade(uint8_t grade)
     return grade_roster;
 }
 
-/* Find first item with given grade. If not found return -1 */
-static int get_grade_start_index(uint8_t grade)
-{
-    for (size_t i = 0; i < roster.count; i++)
-    {
-        if (roster.students[i].grade == grade)
-        {
-            return (int)i;
-        }
-    }
-    return -1;
-}
-
 static int find_place_for_student(char *name, uint8_t grade)
 {
     int place_for_student = -1;
     int grade_start_index = get_grade_start_index(grade);
+
+    /*If the grade is not present find correct place by comparing grades*/
     if (grade_start_index == -1)
     {
         if (grade < roster.students[0].grade)
@@ -90,7 +85,7 @@ static int find_place_for_student(char *name, uint8_t grade)
         }
         else
         {
-            for (size_t i = 0; i < roster.count - 1; i++)
+            for (int i = 0; i < (int)roster.count - 1; i++)
             {
                 if (roster.students[i].grade < grade && roster.students[i + 1].grade > grade)
                 {
@@ -100,6 +95,8 @@ static int find_place_for_student(char *name, uint8_t grade)
             }
         }
     }
+
+    /*If the grade is present find correct place by comparing names*/
     else
     {
         int grade_end_index = get_grade_end_index(grade, grade_start_index);
@@ -126,19 +123,33 @@ static int find_place_for_student(char *name, uint8_t grade)
     return place_for_student;
 }
 
+/* Find first item with given grade. If not found return -1 */
+static int get_grade_start_index(uint8_t grade)
+{
+    for (int i = 0; i < (int)roster.count; i++)
+    {
+        if (roster.students[i].grade == grade)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+/*Find last index with given grade*/
 static int get_grade_end_index(uint8_t grade, int grade_start_index)
 {
     int grade_end_index = grade_start_index;
-    for (size_t i = grade_start_index; i < roster.count - 1; i++)
+    for (int i = grade_start_index; i < (int)roster.count - 1; i++)
     {
         if (roster.students[i + 1].grade != grade)
         {
-            grade_end_index = (int)i;
+            grade_end_index = i;
             break;
         }
-        else if (i == roster.count - 2)
+        else if (i == (int)roster.count - 2)
         {
-            grade_end_index = (int)roster.count - 1;
+            grade_end_index = roster.count - 1;
         }
     }
     return grade_end_index;
