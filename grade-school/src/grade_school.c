fed8dce0d1bc563b592c81e2e32fa1ecef5900bd
dawids21 <dawid.stasiak21@gmail.com>
2020-05-04 17:03:07 +0200

    Rewrite get_grade method

--- grade-school/src/grade_school.c
+++ grade-school/src/grade_school.c
@@ -9,7 +9,8 @@ static roster_t roster = {
 static int find_place_for_student(char *name, uint8_t grade);
 static int get_grade_start_index(uint8_t grade);
 static int get_grade_end_index(uint8_t grade, int grade_start_index);
-static void refresh_cuts(int added_grade);
+static int get_grade_cut_start(uint8_t grade);
+static void refresh_cuts(uint8_t added_grade);
 
 roster_t get_roster(void)
 {
@@ -47,7 +48,7 @@ roster_t get_grade(uint8_t grade)
 {
     roster_t grade_roster;
     memset(&grade_roster, 0, sizeof(roster_t));
-    for (int i = grade_cuts[grade * 2 - 2]; i < grade_cuts[grade * 2 - 1]; i++)
+    for (int i = grade_cuts[get_grade_cut_start(grade)]; i < grade_cuts[get_grade_cut_start(grade) + 1]; i++)
     {
         memcpy(&grade_roster.students[grade_roster.count], &roster.students[i], sizeof(student_t));
         grade_roster.count++;
@@ -61,54 +62,60 @@ static int find_place_for_student(char *name, uint8_t grade)
     int grade_start_index = get_grade_start_index(grade);
 
     /*If the grade is not present find correct place by comparing grades*/
-    if (grade_start_index == -1)
-    {
-        if (grade < roster.students[0].grade)
+    if (grade_cuts[grade * 2 - 2])
+        if (grade_start_index == -1)
         {
-            place_for_student = 0;
-        }
-        else if (grade > roster.students[roster.count - 1].grade)
-        {
-            place_for_student = roster.count;
-        }
-        else
-        {
-            for (int i = 0; i < (int)roster.count - 1; i++)
+            if (grade < roster.students[0].grade)
             {
-                if (roster.students[i].grade < grade && roster.students[i + 1].grade > grade)
+                place_for_student = 0;
+            }
+            else if (grade > roster.students[roster.count - 1].grade)
+            {
+                place_for_student = roster.count;
+            }
+            else
+            {
+                for (int i = 0; i < (int)roster.count - 1; i++)
                 {
-                    place_for_student = i + 1;
-                    break;
+                    if (roster.students[i].grade < grade && roster.students[i + 1].grade > grade)
+                    {
+                        place_for_student = i + 1;
+                        break;
+                    }
                 }
             }
         }
-    }
 
-    /*If the grade is present find correct place by comparing names*/
-    else
-    {
-        int grade_end_index = get_grade_end_index(grade, grade_start_index);
-        if (strcmp(name, roster.students[grade_start_index].name) < 0)
-        {
-            place_for_student = grade_start_index;
-        }
-        else if (strcmp(name, roster.students[grade_end_index].name) > 0)
-        {
-            place_for_student = grade_end_index + 1;
-        }
+        /*If the grade is present find correct place by comparing names*/
         else
         {
-            for (int i = grade_start_index; i < grade_end_index; i++)
+            int grade_end_index = get_grade_end_index(grade, grade_start_index);
+            if (strcmp(name, roster.students[grade_start_index].name) < 0)
             {
-                if (strcmp(name, roster.students[i].name) > 0 && strcmp(name, roster.students[i + 1].name) < 0)
+                place_for_student = grade_start_index;
+            }
+            else if (strcmp(name, roster.students[grade_end_index].name) > 0)
+            {
+                place_for_student = grade_end_index + 1;
+            }
+            else
+            {
+                for (int i = grade_start_index; i < grade_end_index; i++)
                 {
-                    place_for_student = i + 1;
-                    break;
+                    if (strcmp(name, roster.students[i].name) > 0 && strcmp(name, roster.students[i + 1].name) < 0)
+                    {
+                        place_for_student = i + 1;
+                        break;
+                    }
                 }
             }
         }
-    }
     return place_for_student;
+}
+
+static int get_grade_cut_start(uint8_t grade)
+{
+    return grade * 2 - 2;
 }
 
 /* Find first item with given grade. If not found return -1 */
@@ -143,10 +150,9 @@ static int get_grade_end_index(uint8_t grade, int grade_start_index)
     return grade_end_index;
 }
 
-static void refresh_cuts(int added_grade)
+static void refresh_cuts(uint8_t added_grade)
 {
-    grade_cuts[added_grade * 2 - 1]++;
-    for (int i = added_grade * 2; i < MAX_GRADES * 2; i++)
+    for (int i = get_grade_cut_start(added_grade) + 1; i < MAX_GRADES * 2; i++)
     {
         grade_cuts[i]++;
     }
