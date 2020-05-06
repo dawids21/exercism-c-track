0d58a8d6a6a39b4f313eff9881d2270de298d03e
dawids21 <dawid.stasiak21@gmail.com>
2020-05-06 08:15:26 +0200

    Write convert function

--- binary/src/binary.c
+++ binary/src/binary.c
@@ -1,1 +1,21 @@
 #include "binary.h"
+#include "string.h"
+#include "math.h"
+
+int convert(const char *input)
+{
+    int number = 0;
+    for (int i = strlen(input) - 1; i >= 0; i--)
+    {
+        char digit = input[i];
+        if (digit != '1' && digit != '0')
+        {
+            return INVALID;
+        }
+        if (digit == '1')
+        {
+            number += (int)pow(2, strlen(input) - 1 - i);
+        }
+    }
+    return number;
+}
