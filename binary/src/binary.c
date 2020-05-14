63fc711e065c5f48f13c8daef6f741d2ca41d605
dawids21 <dawid.stasiak21@gmail.com>
2020-05-14 14:40:49 +0200

    Add better power function

--- binary/src/binary.c
+++ binary/src/binary.c
@@ -2,6 +2,8 @@
 #include "string.h"
 #include "math.h"
 
+static int power(const int base, const int exponent);
+
 int convert(const char *input)
 {
     int number = 0;
@@ -22,8 +24,18 @@ int convert(const char *input)
              * exponent:    3210
              * So to get exponent function subtract i from strlen-1
              **/
-            number += (int)pow(2, input_len - 1 - i);
+            number += power(2, input_len - 1 - i);
         }
     }
     return number;
+}
+
+static int power(const int base, const int exponent)
+{
+    int result = 1;
+    for (int i = 0; i < exponent; i++)
+    {
+        result *= base;
+    }
+    return result;
 }
