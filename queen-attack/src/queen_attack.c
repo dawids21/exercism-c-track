08a88a9a647c6e65cb678ac64d4a081fd1927114
dawids21 <dawid.stasiak21@gmail.com>
2020-05-18 07:28:15 +0200

    Add checking for invalid postion

--- queen-attack/src/queen_attack.c
+++ queen-attack/src/queen_attack.c
@@ -1,1 +1,11 @@
 #include "queen_attack.h"
+
+attack_status_t can_attack(position_t queen_1, position_t queen_2)
+{
+    if (queen_1.column > 8 || queen_1.row > 8 ||
+        queen_2.column > 8 || queen_2.row > 8 ||
+        (queen_1.row == queen_2.row && queen_1.column == queen_2.column))
+    {
+        return INVALID_POSITION;
+    }
+}
