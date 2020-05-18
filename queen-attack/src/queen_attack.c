94eb500607c2f72b18f9587e3992399a4730e903
dawids21 <dawid.stasiak21@gmail.com>
2020-05-18 07:31:03 +0200

    Replace > with >= in invalid position checking

--- queen-attack/src/queen_attack.c
+++ queen-attack/src/queen_attack.c
@@ -2,10 +2,11 @@
 
 attack_status_t can_attack(position_t queen_1, position_t queen_2)
 {
-    if (queen_1.column > 8 || queen_1.row > 8 ||
-        queen_2.column > 8 || queen_2.row > 8 ||
+    if (queen_1.column >= 8 || queen_1.row >= 8 ||
+        queen_2.column >= 8 || queen_2.row >= 8 ||
         (queen_1.row == queen_2.row && queen_1.column == queen_2.column))
     {
         return INVALID_POSITION;
     }
+    return CAN_ATTACK;
 }
