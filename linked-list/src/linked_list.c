50e417e9d441bb229a5840b25fa942bf8d4eccb3
dawids21 <dawid.stasiak21@gmail.com>
2020-06-05 20:20:39 +0200

    Create function for creating a list

--- linked-list/src/linked_list.c
+++ linked-list/src/linked_list.c
@@ -1,4 +1,5 @@
 #include "linked_list.h"
+#include <stdlib.h>
 
 struct list_item
 {
@@ -6,3 +7,11 @@ struct list_item
     struct list_item *next;
     ll_data_t data;
 };
+
+struct list_item **new_list(void)
+{
+    struct list_item **list = (struct list_item **)malloc(sizeof(struct list_item *) * 2);
+    *list = NULL;
+    *(list + 1) = NULL;
+    return list;
+}
