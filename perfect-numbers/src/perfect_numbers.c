a8987e6a7dbafbd1ad976c1ea7185aec4e058095
dawids21 <dawid.stasiak21@gmail.com>
2020-07-07 10:33:31 +0200

    Create function gor calculating aliqout sum

--- perfect-numbers/src/perfect_numbers.c
+++ perfect-numbers/src/perfect_numbers.c
@@ -1,8 +1,27 @@
 #include "perfect_numbers.h"
 
+static int calculate_aliquot_sum(const int number);
+
 kind classify_number(const int number)
 {
-    (void)number;
+    if (number < 1) {
+        return ERROR;
+    }
     kind type_of_number = ERROR;
+    int aliquot_sum = calculate_aliquot_sum(number);
     return type_of_number;
+}
+
+static int calculate_aliquot_sum(const int number)
+{
+    int sum = 0;
+    for (int i = 1; i < number; i++) {
+        if (i == number) {
+            continue;
+        }
+        if (number % i == 0) {
+            sum += i;
+        }
+    }
+    return sum;
 }
