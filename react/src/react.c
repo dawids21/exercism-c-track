93a9955055c6d53b6bd293fb7bd753d2a8047018
dawids21 <dawid.stasiak21@gmail.com>
2020-07-14 10:39:57 +0200

    Divide reaction structure into one and two

--- react/src/react.c
+++ react/src/react.c
@@ -5,15 +5,22 @@ enum types_of_cell { INPUT,
     COMPUTE_ONE,
     COMPUTE_TWO };
 
-struct reaction_t {
+struct reaction_one_t {
     cell* output_cell;
-    cell* input_cell_1;
-    cell* input_cell_2;
-    compute1 method1;
-    compute2 method2;
+    cell* input_cell;
+    compute1 method;
 };
 
-typedef struct reaction_t reaction;
+typedef struct reaction_one_t reaction_one;
+
+struct reaction_two_t {
+    cell* output_cell;
+    cell* input_cell_one;
+    cell* input_cell_two;
+    compute1 method;
+};
+
+typedef struct reaction_two_t reaction_two;
 
 struct callback_data_t {
     void* input;
@@ -31,6 +38,8 @@ struct cell_t {
 };
 
 struct reactor {
-    reaction** reactions;
-    int num_of_reactions;
+    reaction_one** reactions_one;
+    int num_of_reactions_one;
+    reaction_two** reactions_two;
+    int num_of_reactions_two;
 };
