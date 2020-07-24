8d9cc9af7c367150a5b9981e5e559a46d56ef5dc
dawids21 <dawid.stasiak21@gmail.com>
2020-07-24 11:49:33 +0200

    Write working solution

--- nth-prime/src/nth_prime.c
+++ nth-prime/src/nth_prime.c
@@ -1,1 +1,42 @@
 #include "nth_prime.h"
+#include <stdint.h>
+#include <stdbool.h>
+
+static bool is_prime(uint32_t number);
+
+uint32_t nth(uint32_t n)
+{
+    if (n < 1)
+    {
+        return 0;
+    }
+
+    uint32_t prime = 0;
+    uint32_t num_of_prime = 0;
+    for (uint32_t i = 2; num_of_prime != n; i++)
+    {
+        if (is_prime(i))
+        {
+            prime = i;
+            num_of_prime++;
+        }
+    }
+
+    return prime;
+}
+
+static bool is_prime(uint32_t number)
+{
+    bool prime = true;
+
+    for (uint32_t factor = 2; factor < number; factor++)
+    {
+        if (number % factor == 0)
+        {
+            prime = false;
+            break;
+        }
+    }
+
+    return prime;
+}
