c8edecbc0e46825001799b2f911240b5fd217105
dawids21 <dawid.stasiak21@gmail.com>
2020-07-28 15:55:36 +0200

    Add stub functions

--- triangle/src/triangle.c
+++ triangle/src/triangle.c
@@ -1,1 +1,19 @@
 #include "triangle.h"
+
+bool is_equilateral(triangle_t triangle)
+{
+    (void)triangle;
+    return false;
+}
+
+bool is_isosceles(triangle_t triangle)
+{
+    (void)triangle;
+    return false;
+}
+
+bool is_scalene(triangle_t triangle)
+{
+    (void)triangle;
+    return false;
+}
