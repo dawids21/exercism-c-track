f9b95528225a5faa4ad91b7e0d7157e7fe6c2f59
dawids21 <dawid.stasiak21@gmail.com>
2020-07-28 16:26:42 +0200

    Test if triangle is correct

--- triangle/src/triangle.c
+++ triangle/src/triangle.c
@@ -1,16 +1,42 @@
 #include "triangle.h"
 
+bool is_correct_triangle(triangle_t triangle);
+
 bool is_equilateral(triangle_t triangle)
 {
+    if (!is_correct_triangle(triangle)) {
+        return false;
+    }
+
     return triangle.a == triangle.b && triangle.b == triangle.c;
 }
 
 bool is_isosceles(triangle_t triangle)
 {
+    if (!is_correct_triangle(triangle)) {
+        return false;
+    }
+
     return (triangle.a == triangle.b || triangle.a == triangle.c || triangle.b == triangle.c);
 }
 
 bool is_scalene(triangle_t triangle)
 {
+    if (!is_correct_triangle(triangle)) {
+        return false;
+    }
+
     return triangle.a != triangle.b && triangle.a != triangle.c && triangle.b != triangle.c;
+}
+
+bool is_correct_triangle(triangle_t triangle)
+{
+    bool correct_triangle = true;
+    if (triangle.a == 0 || triangle.b == 0 || triangle.c == 0) {
+        correct_triangle = false;
+    }
+    if (triangle.a + triangle.b < triangle.c || triangle.a + triangle.c < triangle.b || triangle.b + triangle.c < triangle.a) {
+        correct_triangle = false;
+    }
+    return correct_triangle;
 }
