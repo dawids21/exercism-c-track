ae1b49e3d927827fcafb6d6abd4c042290bd99bf
dawids21 <dawid.stasiak21@gmail.com>
2020-07-28 17:21:05 +0200

    Write methods that finds max exponent that is smaller than given number

--- all-your-base/src/all_your_base.c
+++ all-your-base/src/all_your_base.c
@@ -1,4 +1,5 @@
 #include "all_your_base.h"
+#include <math.h>
 
 size_t rebase(int8_t* digits, int16_t input_base, int16_t output_base, size_t input_length)
 {
@@ -7,4 +8,17 @@ size_t rebase(int8_t* digits, int16_t input_base, int16_t output_base, size_t in
     (void)output_base;
     (void)input_length;
     return 0;
+}
+
+int8_t find_max_exponent(int64_t number, int16_t base)
+{
+    int8_t max = 0;
+    for (int i = 1; i < DIGITS_ARRAY_SIZE; i++) {
+        if (number < pow(base, i)) {
+            max = i;
+        } else {
+            break;
+        }
+    }
+    return max;
 }
