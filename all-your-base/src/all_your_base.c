894da7e6745d2c7ae11b923d9420334509fc766a
dawids21 <dawid.stasiak21@gmail.com>
2020-07-28 17:12:15 +0200

    Create stub for function rebase

--- all-your-base/src/all_your_base.c
+++ all-your-base/src/all_your_base.c
@@ -1,1 +1,10 @@
 #include "all_your_base.h"
+
+size_t rebase(int8_t* digits, int16_t input_base, int16_t output_base, size_t input_length)
+{
+    (void)digits;
+    (void)input_base;
+    (void)output_base;
+    (void)input_length;
+    return 0;
+}
