7861559c3f2a2beced67a6485dbab0a6744d8a66
dawids21 <dawid.stasiak21@gmail.com>
2020-07-28 18:30:33 +0200

    Write function for converting to base 10

--- all-your-base/src/all_your_base.c
+++ all-your-base/src/all_your_base.c
@@ -3,6 +3,7 @@
 #include <math.h>
 
 static int64_t convert_number_to_base_10(int8_t* digits, int16_t input_base, size_t length);
+static int64_t power(int8_t base, int8_t exponent);
 static int8_t find_max_exponent(int64_t number, int16_t base);
 
 size_t rebase(int8_t* digits, int16_t input_base, int16_t output_base, size_t input_length)
@@ -12,6 +13,17 @@ size_t rebase(int8_t* digits, int16_t input_base, int16_t output_base, size_t in
 }
 
 static int64_t convert_number_to_base_10(int8_t* digits, int16_t input_base, size_t length)
+{
+    int64_t number = 0;
+    int exponent = length - 1;
+    for (int i = 0; i < length; i++) {
+        number += power(digits[i], exponent);
+        exponent--;
+    }
+    return number;
+}
+
+static int64_t power(int8_t base, int8_t exponent)
 {
     return 0;
 }
