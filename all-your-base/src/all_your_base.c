c16009e1c3e5e155d187aac70409457f9ef92272
dawids21 <dawid.stasiak21@gmail.com>
2020-07-28 17:25:03 +0200

    Stub for function that converts number to base 10

--- all-your-base/src/all_your_base.c
+++ all-your-base/src/all_your_base.c
@@ -1,5 +1,9 @@
 #include "all_your_base.h"
+#include <bits/stdint-intn.h>
 #include <math.h>
+
+static int64_t convert_number_to_base_10(int8_t* digits, size_t length);
+static int8_t find_max_exponent(int64_t number, int16_t base);
 
 size_t rebase(int8_t* digits, int16_t input_base, int16_t output_base, size_t input_length)
 {
@@ -7,6 +11,12 @@ size_t rebase(int8_t* digits, int16_t input_base, int16_t output_base, size_t in
     (void)input_base;
     (void)output_base;
     (void)input_length;
+    int64_t number = convert_number_to_base_10(digits, input_length);
+    return 0;
+}
+
+int64_t convert_number_to_base_10(int8_t* digits, size_t length)
+{
     return 0;
 }
 
