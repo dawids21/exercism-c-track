4b920b8ea547a74ef4523a12055b085e77998b4e
dawids21 <dawid.stasiak21@gmail.com>
2020-07-28 18:22:17 +0200

    Add input base argument for converting to base 10

--- all-your-base/src/all_your_base.c
+++ all-your-base/src/all_your_base.c
@@ -2,20 +2,16 @@
 #include <bits/stdint-intn.h>
 #include <math.h>
 
-static int64_t convert_number_to_base_10(int8_t* digits, size_t length);
+static int64_t convert_number_to_base_10(int8_t* digits, int16_t input_base, size_t length);
 static int8_t find_max_exponent(int64_t number, int16_t base);
 
 size_t rebase(int8_t* digits, int16_t input_base, int16_t output_base, size_t input_length)
 {
-    (void)digits;
-    (void)input_base;
-    (void)output_base;
-    (void)input_length;
-    int64_t number = convert_number_to_base_10(digits, input_length);
+    int64_t number = convert_number_to_base_10(digits, input_base, input_length);
     return 0;
 }
 
-int64_t convert_number_to_base_10(int8_t* digits, size_t length)
+static int64_t convert_number_to_base_10(int8_t* digits, int16_t input_base, size_t length)
 {
     return 0;
 }
