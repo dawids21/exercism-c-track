177e60fa2356d5e220a903232eea6d89b9c33971
dawids21 <dawid.stasiak21@gmail.com>
2020-07-30 19:55:59 +0200

    Working solution

--- largest-series-product/src/largest_series_product.c
+++ largest-series-product/src/largest_series_product.c
@@ -3,7 +3,14 @@
 int64_t largest_series_product(char *digits, size_t span)
 {
     int64_t max = 0;
-    (void)digits;
-    (void)span;
+    for (size_t i = 0; digits[i + span - 1] != '\0'; i++)
+    {
+        int64_t candidate = digits[i] - '0';
+        for (size_t digits_index = 1; digits_index < span; digits_index++)
+        {
+            candidate *= digits[digits_index + i] - '0';
+        }
+        max = candidate > max ? candidate : max;
+    }
     return max;
 }
