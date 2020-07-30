36902ae43e95d8db126c7778da8ac33c2d8b98f2
dawids21 <dawid.stasiak21@gmail.com>
2020-07-30 10:55:03 +0200

    Write function to append palindrome to array

--- palindrome-products/src/palindrome_products.c
+++ palindrome-products/src/palindrome_products.c
@@ -9,6 +9,10 @@ static int is_palindrome_in_array(palindrome_t *array,
                                   int palindrome,
                                   size_t array_length);                  //TODO
 static void add_factors(palindrome_t palindrome, int first, int second); //TODO
+static size_t add_palindrome(palindrome_t *array,
+                             int first_factor,
+                             int second_factor,
+                             size_t array_length);
 
 typedef struct
 {
@@ -58,4 +62,19 @@ static void add_factors(palindrome_t palindrome, int first, int second) //TODO
     factor_t *factors = (factor_t *)calloc(1, sizeof(factor_t));
     factors->factor_a = first;
     factors->factor_b = second;
+}
+
+static size_t add_palindrome(palindrome_t *array,
+                             int first_factor,
+                             int second_factor,
+                             size_t array_length)
+{
+    palindrome_t palindrome;
+    palindrome.number = first_factor * second_factor;
+    palindrome.factors = (factor_t *)calloc(1, sizeof(factor_t));
+    palindrome.factors->factor_a = first_factor;
+    palindrome.factors->factor_b = second_factor;
+    palindrome.factors->next = NULL;
+    array[array_length] = palindrome;
+    return array_length + 1;
 }
