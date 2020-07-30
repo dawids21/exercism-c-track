f6be8054bfa277eb17d3ed2281080142777c7ca4
dawids21 <dawid.stasiak21@gmail.com>
2020-07-30 09:31:21 +0200

    Write method for converting int to char array

--- palindrome-products/src/palindrome_products.c
+++ palindrome-products/src/palindrome_products.c
@@ -1,1 +1,19 @@
 #include "palindrome_products.h"
+#include <math.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+static size_t convert_number_to_char_array(int number, char *output);
+
+static size_t convert_number_to_char_array(int number, char *output)
+{
+    size_t length = (size_t)(log10(number)) + 1;
+    output = (char)calloc(length, sizeof(char));
+    char *current = output;
+    do
+    {
+        *current++ = number % 10;
+        number /= 10;
+    } while (number != 0);
+    return length;
+}
