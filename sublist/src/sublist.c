84e19645e92560c890daa120c854d7e9484a7eb4
dawids21 <dawid.stasiak21@gmail.com>
2020-07-30 18:08:44 +0200

    Create stub of function

--- sublist/src/sublist.c
+++ sublist/src/sublist.c
@@ -1,1 +1,13 @@
 #include "sublist.h"
+
+comparison_result_t check_lists(int *list_to_compare, int *base_list,
+                                size_t list_to_compare_element_count,
+                                size_t base_list_element_count)
+{
+    comparison_result_t result = UNEQUAL;
+    (void)list_to_compare;
+    (void)base_list;
+    (void)list_to_compare_element_count;
+    (void)base_list_element_count;
+    return result;
+}
