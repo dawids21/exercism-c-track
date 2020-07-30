4f44b843bbdd16857c77190f588d967c78cd72bd
dawids21 <dawid.stasiak21@gmail.com>
2020-07-30 20:07:40 +0200

    Control for loop using strlen

--- largest-series-product/src/largest_series_product.c
+++ largest-series-product/src/largest_series_product.c
@@ -17,7 +17,7 @@ int64_t largest_series_product(char *digits, size_t span)
     }
 
     int64_t max = 0;
-    for (size_t i = 0; digits[i + span - 1] != '\0'; i++)
+    for (size_t i = 0; i + span <= strlen(digits); i++)
     {
         int64_t candidate = digits[i] - '0';
         for (size_t digits_index = 1; digits_index < span; digits_index++)
