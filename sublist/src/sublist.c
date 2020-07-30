3b06248c40c66f0ee0ac8c4b920e68b638ec1c3e
dawids21 <dawid.stasiak21@gmail.com>
2020-07-30 19:35:43 +0200

    Write function for checking lists

--- sublist/src/sublist.c
+++ sublist/src/sublist.c
@@ -1,13 +1,35 @@
 #include "sublist.h"
+#include <stdbool.h>
+#include <string.h>
 
+static bool is_sublist(int *list_to_compare, int *base_list,
+                       size_t list_to_compare_len, size_t base_list_len);
 comparison_result_t check_lists(int *list_to_compare, int *base_list,
                                 size_t list_to_compare_element_count,
                                 size_t base_list_element_count)
 {
     comparison_result_t result = UNEQUAL;
-    (void)list_to_compare;
-    (void)base_list;
-    (void)list_to_compare_element_count;
-    (void)base_list_element_count;
+    if (is_sublist(list_to_compare, base_list, list_to_compare_element_count, base_list_element_count))
+    {
+        if (list_to_compare_element_count > base_list_element_count)
+        {
+            result = SUPERLIST;
+        }
+        else if (list_to_compare_element_count == base_list_element_count)
+        {
+            result = EQUAL;
+        }
+        else
+        {
+            result = SUBLIST;
+        }
+    }
     return result;
+}
+
+static bool is_sublist(int *list_to_compare, int *base_list,
+                       size_t list_to_compare_len, size_t base_list_len)
+{
+    bool sublist = false;
+    return sublist;
 }
