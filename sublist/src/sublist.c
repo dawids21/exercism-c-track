09d847590244ed3c365c6f524c1acf4d48cbdcc7
dawids21 <dawid.stasiak21@gmail.com>
2020-07-30 19:35:59 +0200

    Write function is_sublist

--- sublist/src/sublist.c
+++ sublist/src/sublist.c
@@ -30,6 +30,29 @@ comparison_result_t check_lists(int *list_to_compare, int *base_list,
 static bool is_sublist(int *list_to_compare, int *base_list,
                        size_t list_to_compare_len, size_t base_list_len)
 {
+    if (list_to_compare == NULL || base_list == NULL)
+    {
+        return true;
+    }
     bool sublist = false;
+    // swap so that it always checks if smaller list is in bigger
+    if (list_to_compare_len > base_list_len)
+    {
+        int *temp = base_list;
+        base_list = list_to_compare;
+        list_to_compare = temp;
+        size_t temp_len = base_list_len;
+        base_list_len = list_to_compare_len;
+        list_to_compare_len = temp_len;
+    }
+    for (size_t i = 0; i <= base_list_len - list_to_compare_len; i++)
+    {
+        if (!memcmp(list_to_compare, &base_list[i],
+                    list_to_compare_len * sizeof(list_to_compare[0])))
+        {
+            sublist = true;
+            break;
+        }
+    }
     return sublist;
 }
