c688fa6c97e76b019c3c0fc36d74d319225480d2
dawids21 <dawid.stasiak21@gmail.com>
2020-07-31 12:10:39 +0200

    Working solution

--- prime-factors/src/prime_factors.c
+++ prime-factors/src/prime_factors.c
@@ -1,1 +1,21 @@
 #include "prime_factors.h"
+
+size_t find_factors(uint64_t n, uint64_t factors[static MAXFACTORS])
+{
+    size_t num_of_factors = 0;
+    uint64_t input_number = n;
+    for (uint64_t factor = 2; factor <= n; factor++)
+    {
+        if (input_number == 1)
+        {
+            break;
+        }
+        while (input_number % factor == 0)
+        {
+            factors[num_of_factors] = factor;
+            num_of_factors++;
+            input_number /= factor;
+        }
+    }
+    return num_of_factors;
+}
