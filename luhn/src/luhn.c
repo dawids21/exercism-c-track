85aadd33952c2824d2e7ee358c9f6c4971561502
dawids21 <dawid.stasiak21@gmail.com>
2020-07-31 12:40:49 +0200

    Add basic input checking

--- luhn/src/luhn.c
+++ luhn/src/luhn.c
@@ -1,1 +1,12 @@
 #include "luhn.h"
+#include <string.h>
+
+static has_invalid_chars(const char *str);
+
+bool luhn(const char *num)
+{
+    if (has_invalid_chars(num) || strlen(num) <= 1)
+    {
+        return false;
+    }
+}
