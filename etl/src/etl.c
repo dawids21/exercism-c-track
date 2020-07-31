ed8ad25b293e968f0279fce64434de3e8d757faf
dawids21 <dawid.stasiak21@gmail.com>
2020-07-31 07:36:55 +0200

    Write stub function

--- etl/src/etl.c
+++ etl/src/etl.c
@@ -1,1 +1,11 @@
 #include "etl.h"
+
+int convert(const legacy_map *input, const size_t input_len,
+            new_map **output)
+{
+    int output_len = 0;
+    (void)input;
+    (void)input_len;
+    (void)output;
+    return output_len;
+}
