b0009dcce98112b67784b35c7be174c38e504628
dawids21 <dawid.stasiak21@gmail.com>
2020-07-31 12:54:57 +0200

    In input checking check num of digits

--- luhn/src/luhn.c
+++ luhn/src/luhn.c
@@ -2,10 +2,11 @@
 #include <string.h>
 
 static bool has_invalid_chars(const char *str);
+static int count_digits(const char *str);
 
 bool luhn(const char *num)
 {
-    if (has_invalid_chars(num) || strlen(num) <= 1)
+    if (has_invalid_chars(num) || count_digits(num) <= 1)
     {
         return false;
     }
@@ -15,4 +16,10 @@ static bool has_invalid_chars(const char *str)
 {
     bool invalid_chars = false;
     return invalid_chars;
+}
+
+static int count_digits(const char *str)
+{
+    int num_of_digits = 0;
+    return num_of_digits;
 }
