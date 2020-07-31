a52a0bc22cd2fd4a9bb57ef2d7a9e64cbc24e595
dawids21 <dawid.stasiak21@gmail.com>
2020-07-31 13:10:36 +0200

    Debug pointer arithmetic

--- luhn/src/luhn.c
+++ luhn/src/luhn.c
@@ -29,15 +29,15 @@ bool luhn(const char *num)
 static bool has_invalid_chars(const char *str)
 {
     bool invalid_chars = false;
-    char current_char = *str;
+    char const *current_char = str;
     do
     {
-        if (!isdigit(current_char) && current_char != ' ')
+        if (!isdigit(*current_char) && *current_char != ' ')
         {
             invalid_chars = true;
             break;
         }
-    } while (++current_char != '\0');
+    } while (*(++current_char) != '\0');
 
     return invalid_chars;
 }
@@ -45,13 +45,13 @@ static bool has_invalid_chars(const char *str)
 static int count_digits(const char *str)
 {
     int num_of_digits = 0;
-    char current_char = *str;
+    char const *current_char = str;
     do
     {
-        if (isdigit(current_char))
+        if (isdigit(*current_char))
         {
             num_of_digits++;
         }
-    } while (++current_char != '\0');
+    } while (*(++current_char) != '\0');
     return num_of_digits;
 }
