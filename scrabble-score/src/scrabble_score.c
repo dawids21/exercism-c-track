bcc7f8ff449e896073a6417842a9ec7490cebfec
dawids21 <dawid.stasiak21@gmail.com>
2020-07-31 07:25:24 +0200

    Add table with points

--- scrabble-score/src/scrabble_score.c
+++ scrabble-score/src/scrabble_score.c
@@ -1,8 +1,13 @@
 #include "scrabble_score.h"
 
+static const int POINTS[] = { 1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1,
+                              3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4,
+                              8, 4, 10 };
+
 unsigned int score(const char *word)
 {
     unsigned int result = 0;
+    (void)POINTS;
     (void)word;
     return result;
 }
