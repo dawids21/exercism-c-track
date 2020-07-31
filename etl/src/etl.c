b5b1734b943be94d72d866fe2f3e2c226b0520f8
dawids21 <dawid.stasiak21@gmail.com>
2020-07-31 10:35:48 +0200

    Rewrite add_entry func to insert alphabetically

--- etl/src/etl.c
+++ etl/src/etl.c
@@ -5,15 +5,17 @@
 
 static int calculate_output_len(const legacy_map *input,
                                 const size_t input_len);
-static void add_entry(new_map *map, const size_t index,
-                      const char key, const int value);
+static void insert_index(new_map *map, const size_t capacity,
+                         const char key, const int value);
+static void move_elements_by_one(const new_map *map,
+                                 size_t start_index,
+                                 size_t capacity);
 
 int convert(const legacy_map *input, const size_t input_len,
             new_map **output)
 {
     int output_len = calculate_output_len(input, input_len);
     *output = (new_map *)calloc(output_len, sizeof(new_map));
-    size_t output_index = 0;
     for (size_t input_index = 0; input_index < input_len; input_index++)
     {
         int value = input[input_index].value;
@@ -21,10 +23,9 @@ int convert(const legacy_map *input, const size_t input_len,
              keys_index < strlen(input[input_index].keys);
              keys_index++)
         {
-            add_entry(*output, output_index,
-                      input[input_index].keys[keys_index],
-                      value);
-            output_index++;
+            insert_index(*output, output_len,
+                         input[input_index].keys[keys_index],
+                         value);
         }
     }
     return output_len;
@@ -41,9 +42,24 @@ static int calculate_output_len(const legacy_map *input,
     return output_len;
 }
 
-static void add_entry(new_map *map, const size_t index,
-                      const char key, const int value)
+static void insert_index(new_map *map, const size_t capacity,
+                         const char key, const int value)
 {
-    map[index].key = tolower(key);
-    map[index].value = value;
+    char insert_key = tolower(key);
+    for (size_t i = 0; i < capacity; i++)
+    {
+        if (map[i].key == '\0')
+        {
+            map[i].key = insert_key;
+            map[i].value = value;
+            break;
+        }
+        else if (i + 1 < capacity && map[i].key < insert_key && map[i + 1].key > insert_key)
+        {
+            move_elements_by_one(map, i + 1, capacity);
+            map[i + 1].key = insert_key;
+            map[i + 1].value = value;
+            break;
+        }
+    }
 }
