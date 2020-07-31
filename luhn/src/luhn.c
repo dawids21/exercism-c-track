7dfd1179ba37550fdf9cb50ae31e1653b7af735e
dawids21 <dawid.stasiak21@gmail.com>
2020-07-31 12:56:44 +0200

    Write func for counting digits

--- luhn/src/luhn.c
+++ luhn/src/luhn.c
@@ -1,4 +1,5 @@
 #include "luhn.h"
+#include <ctype.h>
 #include <string.h>
 
 static bool has_invalid_chars(const char *str);
@@ -21,5 +22,12 @@ static bool has_invalid_chars(const char *str)
 static int count_digits(const char *str)
 {
     int num_of_digits = 0;
+    for (size_t i = 0; i < strlen(str); i++)
+    {
+        if (isdigit(str[i]))
+        {
+            num_of_digits++;
+        }
+    }
     return num_of_digits;
 }
