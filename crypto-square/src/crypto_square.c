f7dfda80b921827775bf4e5401d7c624542d2ab2
dawids21 <dawid.stasiak21@gmail.com>
2020-07-31 13:49:54 +0200

    Write func to normalize string

--- crypto-square/src/crypto_square.c
+++ crypto-square/src/crypto_square.c
@@ -1,1 +1,23 @@
 #include "crypto_square.h"
+#include <ctype.h>
+#include <string.h>
+
+static size_t normalize_string(const char *input, char *output);
+
+static size_t normalize_string(const char *input, char *output)
+{
+    size_t output_len;
+    for (size_t i = 0; i < strlen(input); i++)
+    {
+        char ch = input[i];
+        if (isalpha(ch))
+        {
+            output[output_len++] = tolower(ch);
+        }
+        else if (isdigit(ch))
+        {
+            output[output_len++] = ch;
+        }
+    }
+    return output_len;
+}
--- crypto-square/src/crypto_square.h
+++ crypto-square/src/crypto_square.h
@@ -1,5 +1,4 @@
 #ifndef CRYPTO_SQUARE_H
-
 #define CRYPTO_SQUARE_H
 
 char *ciphertext(const char *input);
