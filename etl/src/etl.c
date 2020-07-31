5b3e3543ab97bc5c81ee86ead4a31401cea5e2ed
dawids21 <dawid.stasiak21@gmail.com>
2020-07-31 07:43:23 +0200

    Stub for function that calculates output len

--- etl/src/etl.c
+++ etl/src/etl.c
@@ -1,11 +1,20 @@
 #include "etl.h"
 
+static int calculate_output_len(const legacy_map *input,
+                                const size_t input_len);
+
 int convert(const legacy_map *input, const size_t input_len,
             new_map **output)
 {
-    int output_len = 0;
-    (void)input;
-    (void)input_len;
+    int output_len = calculate_output_len(input, input_len);
     (void)output;
+    return output_len;
+}
+
+static int calculate_output_len(const legacy_map *input,
+                                const size_t input_len)
+{
+    //TODO
+    int output_len = 0;
     return output_len;
 }
