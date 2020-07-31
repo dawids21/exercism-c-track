115cc562f171e5eeee6cb22ad13779b89400b44f
dawids21 <dawid.stasiak21@gmail.com>
2020-07-31 12:30:58 +0200

    Refactor code

--- prime-factors/src/prime_factors.c
+++ prime-factors/src/prime_factors.c
@@ -4,18 +4,16 @@ size_t find_factors(uint64_t n, uint64_t factors[static MAXFACTORS])
 {
     size_t num_of_factors = 0;
     uint64_t input_number = n;
-    for (uint64_t factor = 2; factor <= n; factor++)
+    uint64_t current_factor = 2;
+    while (input_number != 1)
     {
-        if (input_number == 1)
+        while (input_number % current_factor == 0)
         {
-            break;
-        }
-        while (input_number % factor == 0)
-        {
-            factors[num_of_factors] = factor;
+            factors[num_of_factors] = current_factor;
             num_of_factors++;
-            input_number /= factor;
+            input_number /= current_factor;
         }
+        current_factor++;
     }
     return num_of_factors;
 }
