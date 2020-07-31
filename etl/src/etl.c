845eba26dfbbfb6f9fc0d5df0bb344b9d5b25fd4
dawids21 <dawid.stasiak21@gmail.com>
2020-07-31 11:41:46 +0200

    Use insert point in inserting entry

--- etl/src/etl.c
+++ etl/src/etl.c
@@ -48,22 +48,13 @@ static void insert_index(new_map *map, const size_t capacity,
                          const char key, const int value)
 {
     char insert_key = tolower(key);
-    for (size_t i = 0; i < capacity; i++)
+    int insert_point = find_insert_point(map, insert_key, capacity);
+    if (map[insert_point].key != '\0')
     {
-        if (map[i].key == '\0')
-        {
-            map[i].key = insert_key;
-            map[i].value = value;
-            break;
-        }
-        else if (i + 1 < capacity && map[i].key < insert_key && map[i + 1].key > insert_key)
-        {
-            move_elements_by_one(map, i + 1, capacity);
-            map[i + 1].key = insert_key;
-            map[i + 1].value = value;
-            break;
-        }
+        move_elements_by_one(map, insert_point, capacity);
     }
+    map[insert_point].key = insert_key;
+    map[insert_point].value = value;
 }
 
 static void move_elements_by_one(new_map *map,
