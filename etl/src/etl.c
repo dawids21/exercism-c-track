2fea18440510fd35cf3341142a5523d42a28888f
dawids21 <dawid.stasiak21@gmail.com>
2020-07-31 07:53:09 +0200

    Stub for adding entry to map

--- etl/src/etl.c
+++ etl/src/etl.c
@@ -3,6 +3,8 @@
 
 static int calculate_output_len(const legacy_map *input,
                                 const size_t input_len);
+static void add_entry(const new_map **map, const size_t index,
+                      const char key, const int value);
 
 int convert(const legacy_map *input, const size_t input_len,
             new_map **output)
@@ -18,4 +20,10 @@ static int calculate_output_len(const legacy_map *input,
     //TODO
     int output_len = 0;
     return output_len;
+}
+
+static void add_entry(const new_map **map, const size_t index,
+                      const char key, const int value)
+{
+    //TODO
 }
