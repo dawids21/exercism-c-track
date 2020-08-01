aff4dc53b34ff3cd646c4e721c1da35f58698fda
dawids21 <dawid.stasiak21@gmail.com>
2020-08-01 07:39:16 +0200

    Stub function

--- run-length-encoding/src/run_length_encoding.c
+++ run-length-encoding/src/run_length_encoding.c
@@ -1,1 +1,16 @@
 #include "run_length_encoding.h"
+#include <stdlib.h>
+
+char *encode(const char *text)
+{
+    char *encoded = NULL;
+    (void)text;
+    return encoded;
+}
+
+char *decode(const char *data)
+{
+    char *decoded = NULL;
+    (void)data;
+    return decoded;
+}
