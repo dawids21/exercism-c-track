58cfd1b7bf6968fbee1f88ff003dfe366806490e
dawids21 <dawid.stasiak21@gmail.com>
2020-08-01 07:50:16 +0200

    For loop to iterate through text to encode

--- run-length-encoding/src/run_length_encoding.c
+++ run-length-encoding/src/run_length_encoding.c
@@ -1,10 +1,14 @@
 #include "run_length_encoding.h"
 #include <stdlib.h>
+#include <string.h>
 
 char *encode(const char *text)
 {
     char *encoded = (char *)calloc(MAX_STRING_LEN, sizeof(char));
-    (void)text;
+    for (size_t i = 0; i < strlen(text) - 1; i++)
+    {
+    }
+
     return encoded;
 }
 
