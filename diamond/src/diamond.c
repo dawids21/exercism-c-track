fb69dfa75e89de5be6008f34cb70513319b5b8eb
dawids21 <dawid.stasiak21@gmail.com>
2020-08-01 10:58:02 +0200

    Alloc mem for diamond

--- diamond/src/diamond.c
+++ diamond/src/diamond.c
@@ -4,7 +4,10 @@
 char **make_diamond(const char letter)
 {
     int length = (letter - 'A') * 2 + 1;
-    char **diamond = NULL;
-    (void)letter;
+    char **diamond = (char **)calloc(length, sizeof(char *));
+    for (int i = 0; i < length; i++)
+    {
+        diamond[i] = (char *)calloc(length + 1, sizeof(char));
+    }
     return diamond;
 }
