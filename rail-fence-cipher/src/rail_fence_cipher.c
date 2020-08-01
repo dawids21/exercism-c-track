49364f3ab85541c6bcc86f7aae5e672ea5bcc618
dawids21 <dawid.stasiak21@gmail.com>
2020-08-01 16:58:32 +0200

    Add stubs for func

--- rail-fence-cipher/src/rail_fence_cipher.c
+++ rail-fence-cipher/src/rail_fence_cipher.c
@@ -1,1 +1,16 @@
 #include "rail_fence_cipher.h"
+
+char *encode(char *text, size_t rails)
+{
+    char *output = NULL;
+    (void)text;
+    (void)rails;
+    return output;
+}
+
+char *decode(char *ciphertext, size_t rails)
+{
+    (void)ciphertext;
+    (void)rails;
+    return NULL;
+}
