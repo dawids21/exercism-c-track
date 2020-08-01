da40d7f4c5a24a0df740bdcecbe7710cbc71eca8
dawids21 <dawid.stasiak21@gmail.com>
2020-08-01 16:59:46 +0200

    Define max len of one rail

--- rail-fence-cipher/src/rail_fence_cipher.c
+++ rail-fence-cipher/src/rail_fence_cipher.c
@@ -1,10 +1,13 @@
 #include "rail_fence_cipher.h"
 
+static const int MAX_RAIL_LENGTH = 30;
+
 char *encode(char *text, size_t rails)
 {
     char *output = NULL;
     (void)text;
     (void)rails;
+    (void)MAX_RAIL_LENGTH;
     return output;
 }
 
