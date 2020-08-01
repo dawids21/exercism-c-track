127e2c7155a773a4f6bfc8bf4478bdb398c28900
dawids21 <dawid.stasiak21@gmail.com>
2020-08-01 16:38:44 +0200

    Some changes in isogram

--- isogram/src/isogram.c
+++ isogram/src/isogram.c
@@ -1,23 +1,22 @@
-#include <stdio.h>
-#include <stdbool.h>
+#include "isogram.h"
 #include <ctype.h>
-
-
+#include <stdbool.h>
+#include <stdio.h>
 
 bool is_isogram(const char phrase[])
 {
     if (phrase == NULL)
         return false;
-    bool alphabet[26] = {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,false, false, false,false, false, false,false, false, false, false};
-    for (int i=0; phrase[i] != '\0'; i++)
+    bool alphabet[26] = { false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false };
+    for (int i = 0; phrase[i] != '\0'; i++)
     {
-		if (isalpha(phrase[i]))
+        if (isalpha(phrase[i]))
         {
-			if (alphabet[tolower(phrase[i])-'a'])
+            if (alphabet[tolower(phrase[i]) - 'a'])
                 return false;
             else
-                alphabet[tolower(phrase[i])-'a'] = true;
-		}
+                alphabet[tolower(phrase[i]) - 'a'] = true;
+        }
     }
     return true;
 }
