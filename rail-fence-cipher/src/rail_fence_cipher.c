d100d8edb100b59e488ca41518c9ea744c796c5a
dawids21 <dawid.stasiak21@gmail.com>
2020-08-01 17:03:34 +0200

    Alloc mem for encoded text

--- rail-fence-cipher/src/rail_fence_cipher.c
+++ rail-fence-cipher/src/rail_fence_cipher.c
@@ -1,13 +1,13 @@
 #include "rail_fence_cipher.h"
+#include <stdlib.h>
+#include <string.h>
 
 static const int MAX_RAIL_LENGTH = 30;
 
 char *encode(char *text, size_t rails)
 {
-    char *output = NULL;
-    (void)text;
-    (void)rails;
-    (void)MAX_RAIL_LENGTH;
+    char *output = (char *)calloc(strlen(text) + 1, sizeof(char));
+    char individual_rails[rails][MAX_RAIL_LENGTH];
     return output;
 }
 
