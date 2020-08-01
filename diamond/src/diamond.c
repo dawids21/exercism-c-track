1cbac903f2d5eec4c5f70c628700ea17b17c2846
dawids21 <dawid.stasiak21@gmail.com>
2020-08-01 11:45:25 +0200

    Write working solution

--- diamond/src/diamond.c
+++ diamond/src/diamond.c
@@ -13,6 +13,27 @@ char **make_diamond(const char letter)
 
     sprintf(diamond[0], "%*c%*c", (length + 1) / 2,
             'A', (length + 1) / 2, '\0');
+
+    char current_letter = 'B';
+    for (int row = 1; row < length - 1; row++)
+    {
+        sprintf(diamond[row], "%*c%*c%*c",
+                (length + 1) / 2 - (current_letter - 'A'),
+                current_letter,
+                2 * (current_letter - 'A'),
+                current_letter,
+                (length + 1) / 2 - (current_letter - 'A'),
+                '\0');
+        if (row < (length - 1) / 2)
+        {
+            current_letter--;
+        }
+        else
+        {
+            current_letter++;
+        }
+    }
+
     if (length > 1)
     {
         sprintf(diamond[length - 1], "%*c%*c", (length + 1) / 2,
