6cba41107b6b5062c8cfd3225f5a5a965a0f8500
dawids21 <dawid.stasiak21@gmail.com>
2020-08-01 08:16:56 +0200

    Iterate through input text in encoding

--- run-length-encoding/src/run_length_encoding.c
+++ run-length-encoding/src/run_length_encoding.c
@@ -9,8 +9,19 @@ static void encode_letter(char ch, int num_of_chars, char *output);
 char *encode(const char *text)
 {
     char *encoded = (char *)calloc(MAX_STRING_LEN, sizeof(char));
+    int num_of_chars = 0;
     for (size_t i = 0; i < strlen(text) - 1; i++)
     {
+        if (text[i] != text[i + 1])
+        {
+            num_of_chars++;
+            encode_letter(text[i], num_of_chars, encoded);
+            num_of_chars = 0;
+        }
+        else
+        {
+            num_of_chars++;
+        }
     }
 
     return encoded;
